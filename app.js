dc3192f1ba6dd48acf30b809cb46236c2f92f430
Vivek <snapstar78@gmail.com>
2022-05-08 20:26:04 +0530

    fix: cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["https://prepintech-api.herokuapp.com"],
   },
 });
 

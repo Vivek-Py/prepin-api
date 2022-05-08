ecb828b03f134029f85d50b50aeb3a89d693c433
Vivek <snapstar78@gmail.com>
2022-05-08 19:10:29 +0530

    feat: real time docs socket

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const uuid = require("uuid");
 
 const User = require("./models/user");
 const Channel = require("./models/channel");
+const Document = require("./models/document");
 
 const authMiddleware = require("./middlewares/authMiddleware");
 const userDataFilter = require("./utils/userDataFilter");
@@ -192,3 +193,36 @@ app.use((req, res) => {
   res.statusCode = 400;
   res.send("Resource doesn't exist");
 });
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
